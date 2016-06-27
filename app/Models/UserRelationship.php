63599c8205de0700a89b6464a27748551987502e
yayugu <yayugu@gmail.com>
2016-06-27 18:36:42 +0900

    hoge

--- app/Models/UserRelationship.php
+++ app/Models/UserRelationship.php
@@ -2,6 +2,7 @@
 
 namespace Tatekae\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 
 class UserRelationship extends \Eloquent
@@ -15,6 +16,45 @@ class UserRelationship extends \Eloquent
     protected $fillable = [
         'user_one_id', 'user_two_id', 'created_by', 'state',
     ];
+
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        $keys = $this->getKeyName();
+        if(!is_array($keys)){
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        foreach($keys as $keyName){
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    /**
+     * Get the primary key value for a save query.
+     *
+     * @param mixed $keyName
+     * @return mixed
+     */
+    protected function getKeyForSaveQuery($keyName = null)
+    {
+        if(is_null($keyName)){
+            $keyName = $this->getKeyName();
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
+    }
 
     public static function create_(int $userCreatedBy, int $other): self
     {
